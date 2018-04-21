18b7e5321161bcd55367ee705bc476b603216172
Alex Espinoza <aespinoza@aspria.net>
2018-04-21 13:20:02 -0400

    Create Search component that handles input

---
+++ react-coin/src/components/common/Search.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Search.css';
+
+class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchQuery: '',
+      firstName: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.state);
+  }
+
+  handleChange(event) {
+    const inputName = event.target.name;
+    const inputValue  = event.target.value;
+
+    // Surrounding input name in brackets lets us not have to hard code value check in if statement
+    // and prevents creation of long conditionals
+    this.setState({
+      [inputName]: inputValue,
+    })
+
+    //if (inputName === 'searchQuery') {
+    //  this.setState({ searchQuery: inputValue });
+    //} else if (inputName === 'firstName') {
+    //  this.setState({ firstName: inputValue });
+    //}
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input name="searchQuery" onChange={this.handleChange} />
+        <input name="firstName" onChange={this.handleChange} />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default Search;
